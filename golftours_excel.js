55b12cc9f198bf588af58bb3b81b371d907f7a0b
SaranyaVelusamy537 <saranya@synergygolf.ie>
2025-10-06 14:34:34 +0100

    Update golftours_excel.js

--- golftours_excel.js
+++ golftours_excel.js
@@ -23,18 +23,18 @@ async function generateExcelWithDynamicItinerary(data) {
 
   // Fill itinerary using fixed row map
   const dayCellMap = [
-    { date: 15, hotel: 15, golf: 16, transport: 17 }, // Day 1
-    { date: 20, hotel: 20, golf: 21, transport: 22 }, // Day 2
-    { date: 25, hotel: 25, golf: 26, transport: 27 }, // Day 3
-    { date: 30, hotel: 30, golf: 31, transport: 32 }, // Day 4
-    { date: 35, hotel: 35, golf: 36, transport: 37 }, // Day 5
-    { date: 40, hotel: 40, golf: 41, transport: 42 }, // Day 6
-    { date: 45, hotel: 45, golf: 46, transport: 47 }, // Day 7
-    { date: 50, hotel: 50, golf: 51, transport: 52 }, // Day 8
-    { date: 55, hotel: 55, golf: 56, transport: 57 }, // Day 9
-    { date: 60, hotel: 60, golf: 61, transport: 62 }, // Day 10
-    { date: 65, hotel: 65, golf: 66, transport: 67 }, // Day 11
-    { date: 70, hotel: 70, golf: 71, transport: 72 }, // Day 12
+    { date: 15, day_of_week:16, hotel: 15, golf: 16, transport: 17 }, // Day 1
+    { date: 20, day_of_week:21, hotel: 20, golf: 21, transport: 22 }, // Day 2
+    { date: 25, day_of_week:26, hotel: 25, golf: 26, transport: 27 }, // Day 3
+    { date: 30, day_of_week:31, hotel: 30, golf: 31, transport: 32 }, // Day 4
+    { date: 35, day_of_week:36, hotel: 35, golf: 36, transport: 37 }, // Day 5
+    { date: 40, day_of_week:41, hotel: 40, golf: 41, transport: 42 }, // Day 6
+    { date: 45, day_of_week:46, hotel: 45, golf: 46, transport: 47 }, // Day 7
+    { date: 50, day_of_week:50, hotel: 50, golf: 51, transport: 52 }, // Day 8
+    { date: 55, day_of_week:56, hotel: 55, golf: 56, transport: 57 }, // Day 9
+    { date: 60, day_of_week:61, hotel: 60, golf: 61, transport: 62 }, // Day 10
+    { date: 65, day_of_week:66, hotel: 65, golf: 66, transport: 67 }, // Day 11
+    { date: 70, day_of_week:71, hotel: 70, golf: 71, transport: 72 }, // Day 12
     // add more if template has more days
   ];
 
